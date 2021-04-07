eb013b3e45d1a118f05c3c45b934e909d1088e90
jonbra <jon.brate@ibv.uio.no>
2021-04-07 13:53:35 +0200

    added argument for new directory

--- copy_and_rename_for_Elixir.sh
+++ copy_and_rename_for_Elixir.sh
@@ -1,16 +1,35 @@
-1. Legge scriptet på .FHI_scripts, deretter cd til ønsket mappe.
-2. Ikke kopier pos og neg kontroll
-3. Bare kopiere de over 97% coverage
-4. Legge inn argument for hvilken mappe i newdir som skal brukes
+# Future additions:
+: <<'END'
+- Ikke kopier pos og neg kontroll
+- Bare kopiere de over 97% coverage
+END
 
-594, 595, 597, 
+# NB! The script only works for Artic samples. Each sample should be in a separate directory named ArticXXX.
+# To run the script you need to be inside a directory which contains sub-directories with fastq-files for each sample. 
+# The fastq files must end with .fastq.gz
+# E.g. For the Illumina Artic run 598 you should be inside the directory called Run598 which is inside Run598_Corona.
 
-# Basedir is the directory called e.g. Run598 which is inside Run598_Corona.
-# Inside basedir are the directories for each sample, e.g. ArticXXX--
+# Run the script with the command: bash ~/.fhiscripts/copy_and_rename_for_Elixir.sh -n "fastq_for_Elixir"
+
+PPOSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+	-n|--newdir)
+	newdir="$2"
+	shift # past argument
+	shift # past value
+	;;	
+esac
+done
+
+# Setting basedir (the directory which contains directories for each sample)
 basedir=$(pwd)
 
 # Newdir is the new location for the renamed files
-newdir="/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-ELIXIRsubmisjon/raw/Run598"
+fullnewdir="/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-ELIXIRsubmisjon/raw/${newdir}"
 
 # The year for the run. Will be part of the filename
 year="2021"
@@ -25,8 +44,8 @@ do
         unique="${R1:9:4}"
 
         # Copy and rename files
-        cp $R1 "${newdir}/${unique}_${year}_R1.fastq.gz"
-        cp $R2 "${newdir}/${unique}_${year}_R2.fastq.gz"
+        cp $R1 "${fullnewdir}/${unique}_${year}_R1.fastq.gz"
+        cp $R2 "${fullnewdir}/${unique}_${year}_R2.fastq.gz"
 
         # Go back and repeat for next sample
         cd "${basedir}"
