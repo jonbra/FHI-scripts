8d3115df98895eef062d6f344fc18171db1fd07d
Jon Bråte <jon.brate@fhi.no>
2022-09-01 13:47:44 +0200

    Create index_fasta.R

---
+++ index_fasta.R
@@ -0,0 +1,5 @@
+library(Rsamtools)
+# Index the fasta file
+# Tror det er mulig å indexe gzippa fil??
+#suppressMessages(indexFa(file = "/media/jonr/SATA6TB/Gisaid/sequences.fasta"))
+indexFa(file = "/home/jonr/Downloads/sequences.fasta")
