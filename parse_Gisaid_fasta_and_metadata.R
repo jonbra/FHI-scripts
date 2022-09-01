9afb389c8acdc162eb16cedfd005427307d3580b
Jon Bråte <jon.brate@fhi.no>
2022-09-01 13:48:12 +0200

    Create parse_Gisaid_fasta_and_metadata.R

---
+++ parse_Gisaid_fasta_and_metadata.R
@@ -0,0 +1,39 @@
+pacman::p_load(tidyverse, Rsamtools, lubridate)
+
+# Open the fasta index connection from the script index_fasta.R
+# There needs to be a file with the same filename but ending with .fai in the folder
+fa <- FaFile("/home/jonr/Downloads/sequences.fasta")
+
+# Make a GRanges object
+gr <- as(seqinfo(fa), "GRanges")
+
+# Load the metadata from the Entire Gisaid database
+metadata_Gisaid <- read_tsv("/home/jonr/Downloads/metadata.tsv")
+
+# Sett minimumsdato
+# Denne trekker 2 måneder fra dagens dato
+#min_date <- Sys.Date() %m-% months(2)
+
+# Filter the metadata
+metadata_filtered <- metadata_Gisaid %>% 
+  # Keep omicron only
+  # filter(str_detect(`Pango lineage`, "BA.*") | str_detect(`Pango lineage`, "B.1.1.529")) %>% 
+  # Drop BA.1
+  # filter(str_detect(`Pango lineage`, "BA.1.*", negate = TRUE))
+  # Keep only BA.5
+  filter(str_detect(`Pango lineage`, "^BA.5.*"))
+
+# Clean up
+rm(metadata_Gisaid)
+
+# Match the names of the metadata with the fasta headers
+new_gr <- getSeq(fa, gr[which(gsub("\\|.*", "", names(gr)) %in% metadata_filtered$`Virus name`)])
+
+# Write files
+outfile <- paste0("/home/jonr/Downloads/", Sys.Date(), "_Gisaid.fasta")
+# Write the fasta file
+writeXStringSet(new_gr, outfile, format = "fasta")
+# Write metadata
+write_tsv(metadata_filtered, paste0("/home/jonr/Downloads/", Sys.Date(), "_Gisaid.metadata.tsv"))
+
+# Now, go to the script "get_data_from_BN.R" without removing any objects or variables from the environment
