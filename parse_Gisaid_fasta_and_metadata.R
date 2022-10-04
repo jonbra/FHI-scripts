9191fd9854a30e72728d85543c53c4bfcdf6bfda
Jon Bråte <jon.brate@fhi.no>
2022-10-04 14:10:46 +0200

    Including BA.2.75

--- parse_Gisaid_fasta_and_metadata.R
+++ parse_Gisaid_fasta_and_metadata.R
@@ -20,8 +20,8 @@ metadata_filtered <- metadata_Gisaid %>%
   # filter(str_detect(`Pango lineage`, "BA.*") | str_detect(`Pango lineage`, "B.1.1.529")) %>% 
   # Drop BA.1
   # filter(str_detect(`Pango lineage`, "BA.1.*", negate = TRUE))
-  # Keep only BA.5
-  filter(str_detect(`Pango lineage`, "^BA.5.*") | str_detect(`Pango lineage`, "^BE.*") | str_detect(`Pango lineage`, "^BK.*") | str_detect(`Pango lineage`, "^BF.*") | str_detect(`Pango lineage`, "^BQ.*"))
+  # Keep BA.5 and BA.2.75*
+  filter(str_detect(`Pango lineage`, "^BA.2.75.*") | str_detect(`Pango lineage`, "^BA.5.*") | str_detect(`Pango lineage`, "^BE.*") | str_detect(`Pango lineage`, "^BK.*") | str_detect(`Pango lineage`, "^BF.*") | str_detect(`Pango lineage`, "^BQ.*"))
 
 # Clean up
 rm(metadata_Gisaid)
