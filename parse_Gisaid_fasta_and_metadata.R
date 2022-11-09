67691d34460d9b344781cb991e3cdeee547ecd16
Jon Bråte <jon.brate@fhi.no>
2022-11-09 14:05:49 +0100

    Added several new BA.5 lineages

--- parse_Gisaid_fasta_and_metadata.R
+++ parse_Gisaid_fasta_and_metadata.R
@@ -21,7 +21,28 @@ metadata_filtered <- metadata_Gisaid %>%
   # Drop BA.1
   # filter(str_detect(`Pango lineage`, "BA.1.*", negate = TRUE))
   # Keep BA.5 and BA.2.75*
-  filter(str_detect(`Pango lineage`, "^BA.5.*") | str_detect(`Pango lineage`, "^BE.*") | str_detect(`Pango lineage`, "^BK.*") | str_detect(`Pango lineage`, "^BF.*") | str_detect(`Pango lineage`, "^BQ.*") | str_detect(`Pango lineage`, "^BV.*") | str_detect(`Pango lineage`, "^CF.*"))
+  filter(str_detect(`Pango lineage`, "^BA.5.*") | 
+         str_detect(`Pango lineage`, "^BE.*") | 
+         str_detect(`Pango lineage`, "^BK.*") | 
+         str_detect(`Pango lineage`, "^BT.*") |
+         str_detect(`Pango lineage`, "^BF.*") | 
+         str_detect(`Pango lineage`, "^BQ.*") | 
+         str_detect(`Pango lineage`, "^BW.*") |
+         str_detect(`Pango lineage`, "^BV.*") |
+         str_detect(`Pango lineage`, "^BZ.*") | 
+         str_detect(`Pango lineage`, "^CC.*") |
+         str_detect(`Pango lineage`, "^CF.*") |
+         str_detect(`Pango lineage`, "^CP.*") |
+         str_detect(`Pango lineage`, "^CT.*") |
+         str_detect(`Pango lineage`, "^CY.*") |
+         str_detect(`Pango lineage`, "^DE.*") |
+         str_detect(`Pango lineage`, "^CU.*") |
+         str_detect(`Pango lineage`, "^CL.*") |
+         str_detect(`Pango lineage`, "^CW.*") |
+         str_detect(`Pango lineage`, "^CQ.*") |
+         str_detect(`Pango lineage`, "^CZ.*") |
+         str_detect(`Pango lineage`, "^DA.*") |
+         str_detect(`Pango lineage`, "^DF.*"))
 
 # Clean up
 rm(metadata_Gisaid)
