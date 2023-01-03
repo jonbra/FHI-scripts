4534c4e09d986f7b7cb6aa96720076cb8bfb4ba4
Jon Bråte <jon.brate@fhi.no>
2023-01-03 13:33:23 +0100

    Removed unnecessary code

--- parse_pangolineages.R
+++ parse_pangolineages.R
@@ -6,18 +6,6 @@ library(tidyverse)
 # Read lineage descriptions from GitHub
 pango <- read_delim(file = "https://raw.githubusercontent.com/cov-lineages/pango-designation/master/lineage_notes.txt")
 
-# Extract only BA.5
-pango %>% 
-  # Get the BA.5's
-  filter(str_detect(Description, "B.1.1.529.5")) %>% 
-  # Drop BA.5 for now
-  filter(Lineage != "BA.5") %>% 
-  # Get the abbreviations
-  mutate(tmp = str_sub(Lineage, 1, 3)) %>% 
-  distinct(tmp) %>% 
-  arrange(tmp) %>% print(n = 1000)
-
-
 # Create list of BA.5 lineages for the Nextstrain build file
 pango_str <- pango %>% 
   # Get the BA.5's
