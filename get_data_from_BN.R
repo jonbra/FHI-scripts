cae7da34aa9dd096ffb5d83724069e1e76b5f1b1
Jon Bråte <jon.brate@fhi.no>
2023-02-14 14:18:02 +0100

    Updated BA.5 pangos

--- get_data_from_BN.R
+++ get_data_from_BN.R
@@ -91,7 +91,13 @@ SEQUENCEID_virus_mapping_FHI <- BN %>%
          str_detect(PANGOLIN_NOM, "^DU.*") |
          str_detect(PANGOLIN_NOM, "^DW.*") |
          str_detect(PANGOLIN_NOM, "^DY.*") |
-         str_detect(PANGOLIN_NOM, "^DZ.*")) %>%
+         str_detect(PANGOLIN_NOM, "^DZ.*") |
+         str_detect(PANGOLIN_NOM, "^EA.*") |
+         str_detect(PANGOLIN_NOM, "^EB.*") |
+         str_detect(PANGOLIN_NOM, "^EC.*") |
+         str_detect(PANGOLIN_NOM, "^ED.*") |
+         str_detect(PANGOLIN_NOM, "^EE.*") |
+         str_detect(PANGOLIN_NOM, "^EF.*")) %>%
   # Keep omicron only
   #filter(str_detect(PANGOLIN_NOM, "BA.*") | str_detect(PANGOLIN_NOM, "B.1.1.529")) %>% 
   # Drop BA.1
@@ -121,7 +127,7 @@ SEQUENCEID_virus_mapping_MIK <- BN %>%
   filter(PROVE_TATT >= "2022-01-01") %>% 
   # Keep BA.5 
     # filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | str_detect(PANGOLIN_NOM, "^BE.*") | str_detect(PANGOLIN_NOM, "^BK.*") | str_detect(PANGOLIN_NOM, "^BF.*") | str_detect(PANGOLIN_NOM, "^BQ.*")  | str_detect(PANGOLIN_NOM, "^BV.*") | str_detect(PANGOLIN_NOM, "^CF.*")) %>% 
-   filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
+  filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
          str_detect(PANGOLIN_NOM, "^BE.*") | 
          str_detect(PANGOLIN_NOM, "^BF.*") | 
          str_detect(PANGOLIN_NOM, "^BK.*") |
@@ -165,7 +171,13 @@ SEQUENCEID_virus_mapping_MIK <- BN %>%
          str_detect(PANGOLIN_NOM, "^DU.*") |
          str_detect(PANGOLIN_NOM, "^DW.*") |
          str_detect(PANGOLIN_NOM, "^DY.*") |
-         str_detect(PANGOLIN_NOM, "^DZ.*")) %>%
+         str_detect(PANGOLIN_NOM, "^DZ.*") |
+         str_detect(PANGOLIN_NOM, "^EA.*") |
+         str_detect(PANGOLIN_NOM, "^EB.*") |
+         str_detect(PANGOLIN_NOM, "^EC.*") |
+         str_detect(PANGOLIN_NOM, "^ED.*") |
+         str_detect(PANGOLIN_NOM, "^EE.*") |
+         str_detect(PANGOLIN_NOM, "^EF.*")) %>%
   # Keep omicron only
   #filter(str_detect(PANGOLIN_NOM, "BA.*") | str_detect(PANGOLIN_NOM, "B.1.1.529")) %>% 
   # Drop BA.1
@@ -190,7 +202,7 @@ SEQUENCEID_virus_mapping_Artic <- BN %>%
   filter(PROVE_TATT >= "2022-01-01") %>% 
   # Keep BA.5 
     # filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | str_detect(PANGOLIN_NOM, "^BE.*") | str_detect(PANGOLIN_NOM, "^BK.*") | str_detect(PANGOLIN_NOM, "^BF.*") | str_detect(PANGOLIN_NOM, "^BQ.*")  | str_detect(PANGOLIN_NOM, "^BV.*") | str_detect(PANGOLIN_NOM, "^CF.*")) %>% 
-   filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
+  filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
          str_detect(PANGOLIN_NOM, "^BE.*") | 
          str_detect(PANGOLIN_NOM, "^BF.*") | 
          str_detect(PANGOLIN_NOM, "^BK.*") |
@@ -234,7 +246,13 @@ SEQUENCEID_virus_mapping_Artic <- BN %>%
          str_detect(PANGOLIN_NOM, "^DU.*") |
          str_detect(PANGOLIN_NOM, "^DW.*") |
          str_detect(PANGOLIN_NOM, "^DY.*") |
-         str_detect(PANGOLIN_NOM, "^DZ.*")) %>%
+         str_detect(PANGOLIN_NOM, "^DZ.*") |
+         str_detect(PANGOLIN_NOM, "^EA.*") |
+         str_detect(PANGOLIN_NOM, "^EB.*") |
+         str_detect(PANGOLIN_NOM, "^EC.*") |
+         str_detect(PANGOLIN_NOM, "^ED.*") |
+         str_detect(PANGOLIN_NOM, "^EE.*") |
+         str_detect(PANGOLIN_NOM, "^EF.*")) %>%
 # Keep omicron only
   #filter(str_detect(PANGOLIN_NOM, "BA.*") | str_detect(PANGOLIN_NOM, "B.1.1.529")) %>% 
   # Drop BA.1
@@ -261,7 +279,7 @@ SEQUENCEID_virus_mapping_Nano <- BN %>%
   filter(PROVE_TATT >= "2022-01-01") %>% 
   # Keep BA.5 
     # filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | str_detect(PANGOLIN_NOM, "^BE.*") | str_detect(PANGOLIN_NOM, "^BK.*") | str_detect(PANGOLIN_NOM, "^BF.*") | str_detect(PANGOLIN_NOM, "^BQ.*")  | str_detect(PANGOLIN_NOM, "^BV.*") | str_detect(PANGOLIN_NOM, "^CF.*")) %>% 
-    filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
+   filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
          str_detect(PANGOLIN_NOM, "^BE.*") | 
          str_detect(PANGOLIN_NOM, "^BF.*") | 
          str_detect(PANGOLIN_NOM, "^BK.*") |
@@ -305,7 +323,13 @@ SEQUENCEID_virus_mapping_Nano <- BN %>%
          str_detect(PANGOLIN_NOM, "^DU.*") |
          str_detect(PANGOLIN_NOM, "^DW.*") |
          str_detect(PANGOLIN_NOM, "^DY.*") |
-         str_detect(PANGOLIN_NOM, "^DZ.*")) %>%
+         str_detect(PANGOLIN_NOM, "^DZ.*") |
+         str_detect(PANGOLIN_NOM, "^EA.*") |
+         str_detect(PANGOLIN_NOM, "^EB.*") |
+         str_detect(PANGOLIN_NOM, "^EC.*") |
+         str_detect(PANGOLIN_NOM, "^ED.*") |
+         str_detect(PANGOLIN_NOM, "^EE.*") |
+         str_detect(PANGOLIN_NOM, "^EF.*")) %>%
   # Keep omicron only
   #filter(str_detect(PANGOLIN_NOM, "BA.*") | str_detect(PANGOLIN_NOM, "B.1.1.529")) %>% 
   # Drop BA.1
@@ -594,7 +618,7 @@ eksterne_meta <- BN %>%
   filter(PROVE_TATT >= "2022-01-01") %>% 
   # Keep BA.5
     # filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | str_detect(PANGOLIN_NOM, "^BE.*") | str_detect(PANGOLIN_NOM, "^BK.*") | str_detect(PANGOLIN_NOM, "^BF.*") | str_detect(PANGOLIN_NOM, "^BQ.*")  | str_detect(PANGOLIN_NOM, "^BV.*") | str_detect(PANGOLIN_NOM, "^CF.*")) %>% 
-   filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
+  filter(str_detect(PANGOLIN_NOM, "^BA.5.*") | 
          str_detect(PANGOLIN_NOM, "^BE.*") | 
          str_detect(PANGOLIN_NOM, "^BF.*") | 
          str_detect(PANGOLIN_NOM, "^BK.*") |
@@ -638,7 +662,13 @@ eksterne_meta <- BN %>%
          str_detect(PANGOLIN_NOM, "^DU.*") |
          str_detect(PANGOLIN_NOM, "^DW.*") |
          str_detect(PANGOLIN_NOM, "^DY.*") |
-         str_detect(PANGOLIN_NOM, "^DZ.*")) %>%
+         str_detect(PANGOLIN_NOM, "^DZ.*") |
+         str_detect(PANGOLIN_NOM, "^EA.*") |
+         str_detect(PANGOLIN_NOM, "^EB.*") |
+         str_detect(PANGOLIN_NOM, "^EC.*") |
+         str_detect(PANGOLIN_NOM, "^ED.*") |
+         str_detect(PANGOLIN_NOM, "^EE.*") |
+         str_detect(PANGOLIN_NOM, "^EF.*")) %>%
   # Keep omicron only
   #filter(str_detect(PANGOLIN_NOM, "BA.*") | str_detect(PANGOLIN_NOM, "B.1.1.529")) %>% 
   # Drop BA.1
